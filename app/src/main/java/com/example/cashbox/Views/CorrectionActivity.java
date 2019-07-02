8075a76bf576ce85b2923df27ca6e7a3b1401a4c
1 <bobkov.yura2012@yandex.ru>
2019-07-02 10:14:57 +0300

    Чек коррекции форма

--- app/src/main/java/com/example/cashbox/Views/CorrectionActivity.java
+++ app/src/main/java/com/example/cashbox/Views/CorrectionActivity.java
@@ -1,8 +1,10 @@
-package com.example.cashbox;
+package com.example.cashbox.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.cashbox.R;
 
 public class CorrectionActivity extends AppCompatActivity {
 
